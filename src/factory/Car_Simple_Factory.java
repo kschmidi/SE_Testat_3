429a9e4775b60524eacfd9eb90b70767ff84d222
kschmidi <kschmidi@hsr.ch>
2014-12-05 13:57:32 +0100

    finished the factory class

--- src/factory/Car_Simple_Factory.java
+++ src/factory/Car_Simple_Factory.java
@@ -3,23 +3,27 @@ package factory;
 import car.Car;
 import car.Car_Template;
 import car.decorator.decorators.Aircondition;
+import car.decorator.decorators.Panorama;
 import car.decorator.decorators.Seat_Heat;
 
 public class Car_Simple_Factory{
 	
+	private static String default_Text = "A nice Car";
+	private static int default_Price = 20000;
+	
 	public static Car_Template make_Car_With_AC_And_Panorama() {
-		return new Seat_Heat(new Aircondition(new Car("A nice blue Car", 20000)));
+		return new Aircondition(new Panorama(new Car(default_Text, default_Price)));
 	}
 	
-	public static Car make_Car_With_AC_And_Panorama_And_Seat_Heat() {
-		return null;
+	public static Car_Template make_Car_With_AC_And_Panorama_And_Seat_Heat() {
+		return new Aircondition(new Seat_Heat(new Panorama(new Car (default_Text, default_Price))));
 	}
 	
-	public static Car make_Car_With_AC_And_Seat_Heat() {
-		return null;
+	public static Car_Template make_Car_With_AC_And_Seat_Heat() {
+		return new Aircondition(new Seat_Heat(new Car(default_Text, default_Price)));
 	}
 	
-	public static Car make_Car_With_Seat_Heat_And_Panorama() {
-		return null;
+	public static Car_Template make_Car_With_Seat_Heat_And_Panorama() {
+		return new Seat_Heat(new Panorama(new Car(default_Text, default_Price)));
 	}
 }
